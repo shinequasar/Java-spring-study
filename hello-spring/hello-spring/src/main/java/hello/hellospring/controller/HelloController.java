e548df6360ce09b565a0416732fa1a393be826a0
shinequasar <shinequasar@gmail.com>
2021-04-05 22:06:08 +0900

    [Add] hello-template.html

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,9 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,5 +16,15 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 
+    @GetMapping("hello-string")
+    @ResponseBody //http body에 직접 넣어주겠다 라는 의미
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+ name;
+    }
 }
