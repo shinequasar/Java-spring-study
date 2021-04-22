5af4d45d2594a5924acdc42c07a772ec8d5d6e5f
shinequasar <shinequasar@gmail.com>
2021-04-23 00:41:24 +0900

    [Add] @Componant Scan 방식으로 스프링 빈 구현

--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Members;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MemberRepository {
     Members save(Members member);
     Optional<Members> findById(Long id);
--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Members;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     //Ctrl+Shift+T
     private final MemberRepository memberRepository;
 
+    @Autowired
     //DI 구조( MemberServiceTest 클래스랑 연관됨.)
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
